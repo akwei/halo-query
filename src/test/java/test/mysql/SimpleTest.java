e03e1cd90b5e083f9f4bee41fe43e017c4987223
yuanwei <yuanwei@5wei.com>
2016-12-20 14:17:05 +0800

    增加简单查询测试

---
+++ src/test/java/test/mysql/SimpleTest.java
@@ -0,0 +1,29 @@
+package test.mysql;
+
+import halo.query.Query;
+import halo.query.dal.DALStatus;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import test.bean.TbUser;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/query-testsimple.xml"})
+public class SimpleTest {
+
+    @Autowired
+    private Query query;
+
+    /**
+     * 事务的slave查询需要走master数据源
+     *
+     * @throws Exception
+     */
+    @Test
+    public void getFromSlave() throws Exception {
+        DALStatus.addParam("userId", 1);
+        query.objById(TbUser.class, 1);
+    }
+}
